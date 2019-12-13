2026e5a91425a7b1f79f5f3470f509ede16b7f97
Mask Järn <47307674+Jernkastel@users.noreply.github.com>
2019-12-13 11:44:38 +0100

    Compied some stuff from formsbranch. Have to see if everything works.

--- MCO_Juryplattform/Controllers/QuestionController.cs
+++ MCO_Juryplattform/Controllers/QuestionController.cs
@@ -9,44 +9,73 @@ namespace MCO_Juryplattform.Controllers
 {
     public class QuestionController : Controller
     {
-        [Authorize]
+
         [HttpGet]
         public ActionResult Index(int id)
         {
-            return View(test(id));
+            return View(Viewqestionmodel(id));
         }
 
         [HttpPost]
-        public ActionResult Getting(CompanyQuestions test)
+        public ActionResult Getting(CompanyQuestions anwsers)
         {
-            Console.Write("dlökjf");
-            return View();
+            AddResults(anwsers);
+            AddSubmitvote(anwsers);
+            return RedirectToAction("Index", "Company");
+        }
+        private void AddResults(CompanyQuestions test)
+        {
+            using (JuryModel db = new JuryModel())
+            {
+                for (int i = 0; i < test.Grades.Count; i++)
+                {
+                    Result newResult = new Result();
+                    newResult.CompanyId = test.CompanyId;
+                    newResult.QuestionId = test.Grades[i].QuestionId;
+                    newResult.Grade = test.Grades[i].Answer;
+                    db.Result.Add(newResult);
+                }
+                var temp = db.Result;
+                db.SaveChanges();
+            }
+        }
+        private void AddSubmitvote(CompanyQuestions test)
+        {
+            using (JuryModel db = new JuryModel())
+            {
+                VoteCheck temp = new VoteCheck();
+                temp.CompanyId = test.CompanyId;
+                temp.IsVoted = true;
+                //temp.UserId= 
+
+                db.VoteCheck.Add(temp);
+                //db.SaveChanges();
+            }
         }
 
-        
-        private CompanyQuestions test(int cosencompany)
+        private CompanyQuestions Viewqestionmodel(int cosencompany)
         {
-            var tempform = new List<Form>();
+            var tempform = new List<Grade>();
             CompanyQuestions form;
             using (JuryModel db = new JuryModel())
             {
                 foreach (var item in db.FormQuestions.ToList())
                 {
-                    tempform.Add(new Form
-                    {
-                        Question = item.Question,
-                        FormId = item.Id
-                    });
+                    var temp = new Grade();
+
+                    temp.Question = item.Question;
+                    temp.QuestionId = item.Id;
+                    tempform.Add(temp);
                 }
                 form = new CompanyQuestions
                 {
                     Companyname = db.Company.Find(cosencompany).Name,
                     CompanyId = cosencompany,
-                    Forms = tempform
+                    Grades = tempform
                 };
                 return form;
             }
-           
+
         }
     }
 }
