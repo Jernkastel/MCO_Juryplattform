d961ff7a9b1b6d2a469a2bb9381638871e219a78
Erik Jiremalm <erji0002@student.hv.se>
2019-12-25 11:32:27 +0100

    la till möjlighet att visa kategori samt Authorize

--- MCO_Juryplattform/Controllers/resultatController.cs
+++ MCO_Juryplattform/Controllers/resultatController.cs
@@ -10,15 +10,12 @@ namespace MCO_Juryplattform.Controllers
     public class ResultatController : Controller
     {
         // GET: resultat
+        [Authorize]
         public ActionResult Index()
         {
             List<ShowResult> viewModel = new List<ShowResult>();
             using (JuryModel db = new JuryModel())
             {
-                //"select CompanyId form Result whare"
-                //"select CompanyId form Result whare Result.CompanyId=i"
-                
-               
                 List<Company> allCompanys = db.Company.ToList();
                 List<FormQuestions> allQuestions = db.FormQuestions.ToList();
                 for (int i = 0; i < allCompanys.Count; i++)
@@ -45,18 +42,54 @@ namespace MCO_Juryplattform.Controllers
                     foo.Id = i;
                     viewModel.Add(foo);
                 }
-                //foreach (var item in cmopanyid)
-                //{
-                //    var allResult2 = db.Result.ToList().Where(e => e.CompanyId == item);
-                //}
-                //select graid form Result Where Result.CompanyId == cmtanyid[i], Result.QuestionId == QuestionId[j]
-                //Where(e => e.QuestionId == parameter)
-                //var test = new Result();
-                //test.CompanyId
-                //List< Result > name = db.Result.ToList().Where;
+            }
+
+            viewModel.Sort((x, y) => y.totallpointspercentage.CompareTo(x.totallpointspercentage));
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult KatagoryResult(string katagory)
+        {
+            if(katagory == "ingen"|| katagory ==null || katagory == "")
+            {
+                return RedirectToAction("Index", "Resultat");
+            }
+            List<ShowResult> viewModel = new List<ShowResult>();
+            using (JuryModel db = new JuryModel())
+            {
+                List<Company> allCompanys = db.Company.ToList();
+                List<FormQuestions> allQuestions = db.FormQuestions.ToList().Where(question => question.Category == katagory).ToList();
+                for (int i = 0; i < allCompanys.Count; i++)
+                {
+                    ShowResult foo = new ShowResult();
+                    for (int j = 0; j < allQuestions.Count; j++)
+                    {
+                        List<Result> test = db.Result.ToList().Where(result => result.CompanyId == allCompanys[i].Id && result.QuestionId == allQuestions[j].Id).ToList();
+                        if (test == null) continue;
+                        if (foo.howMenyVoted == 0)
+                        {
+                            foo.howMenyVoted = test.Count;
+                        }
+                        foreach (var item in test)
+                        {
+                            foo.totallpoints += item.Grade;
+                        }
+
+                    }
+                    foo.totallpointspercentage = 100.0 * foo.totallpoints / (5 * allQuestions.Count * foo.howMenyVoted);
+
+                    foo.Companyname = allCompanys[i].Name;
+                    foo.Id = i;
+                    viewModel.Add(foo);
+                }
+            }
+            viewModel.Sort((x, y) => y.totallpointspercentage.CompareTo(x.totallpointspercentage));
+            if (viewModel.Count == 0)
+            {
+                return RedirectToAction("Index", "Resultat");
             }
             return View(viewModel);
-            
         }
     }
 }
